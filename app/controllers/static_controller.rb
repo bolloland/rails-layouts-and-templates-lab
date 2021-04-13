e935d3512a0eb497460f19f0abf23896c860e326
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-04-12 22:25:30 -0500

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,29 @@
+class StaticController < ApplicationController
+  def home
+    # render action: "home", layout: "home"   
+  end
+ 
+  # Create a home view with an h2 that says "Welcome to Flatiron Widgets" 
+  # and a new action in StaticController called home.
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
